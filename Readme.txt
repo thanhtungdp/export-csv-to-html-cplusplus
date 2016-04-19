0f6ac14ab7b538b38d62feb35646d6ce3d83dbc7
thanhtungdp <tungptkh@gmail.com>
2016-04-19 14:44:42 +0700

    update source code and doc

---
+++ Readme.txt
@@ -0,0 +1,29 @@
+# Project Export CSV to HTML
+Đồ án giữa học kỳ - trường đại học Khoa học tự nhiên
+Đồ án án được viết trên nền tảng linux, sử dụng thư viện g++ để biên dịch
+
+# Author
+Tên: Phan Thanh Tùng
+MSSV: 1512654
+SDT: 0162237832
+Email: tungptkh@gmail.com
+Source git: https://github.com/thanhtungdp/export-csv-to-html-cplusplus
+
+# Project info
+Chỉnh sửa file đầu vào: `data/students.csv`
+Chỉnh sửa template: `data/templates/profile.html`
+Thư mục xuất file html: `public`
+Thư mục chứa thư viện: `libs`
+
+# Docs
+
+
+# Biên dịch và chạy chương tình từ github:
+
+`git clone https://github.com/thanhtungdp/export-csv-to-html-cplusplus.git demo_export`
+
+`cd demo_export`
+
+`g++ main.cpp -o main`
+
+`./main`
---
+++ Source/libs/read_csv.h
@@ -0,0 +1,119 @@
+//
+// Created by Phan Tùng on 4/1/16.
+//
+
+#ifndef DO_AN_CUOI_KY_READ_CSV_H
+#define DO_AN_CUOI_KY_READ_CSV_H
+
+#include <iostream>
+#include <fstream>
+#include "memory.h"
+
+using namespace std;
+const string file_src = "data/students.csv";
+const char file_string_delimiter = ',';
+const char file_string_quote = '\"';
+
+/**
+ * Next read index read delimiter
+ * Example: string, test
+ *                ^
+ * Move ^ to next index
+ * To: string, test
+ *            ^
+ */
+void readCSV_NextIndexReadDelimiter(string line, int &index_read) {
+    while (line[index_read] != file_string_delimiter && index_read < line.length()) {
+        index_read++;
+    }
+    index_read++;
+}
+
+/**
+ * Next read index over space to character
+ * Example:"   a "
+ *          ^
+ * Move ^ to a
+ * After: "   a "
+ *            ^
+ */
+void readCSV_NextIndexReadSpace(string line, int &index_read) {
+    while (line[index_read] == ' ' && index_read < line.length()) {
+        index_read++;
+    }
+}
+
+/**
+ * Read variable have quote from string, and move index_read to new
+ * Example: "test 1, tung thanh", "ok baby"
+ * Ouput: (test 1, tung thanh) ; (ok baby)
+ */
+void readCSV_VariableQuote(const string line, int &index_read, string &string_read) {
+    string_read = "";
+    index_read++;
+    while (line[index_read] != file_string_quote && index_read < line.length()) {
+        string_read += line[index_read];
+        index_read++;
+    }
+    readCSV_NextIndexReadDelimiter(line, index_read);
+    readCSV_NextIndexReadSpace(line, index_read);
+}
+
+/**
+ * Read variable normal from string, and move index_read to new
+ * Example: hello world, hello one
+ * Ouput: hello world ; hello one
+ */
+void readCSV_VariableNormal(const string line, int &index_read, string &string_read) {
+    string_read = "";
+    while (line[index_read] != file_string_delimiter && index_read < line.length()) {
+        string_read += line[index_read];
+        index_read++;
+    }
+    readCSV_NextIndexReadDelimiter(line, index_read);
+    readCSV_NextIndexReadSpace(line, index_read);
+}
+
+/**
+ * Read variable from line:
+ * Example: line = "phan thanh tung","van tuan","obama"
+ * With:    index_read = 0 => string_read = "phan thanh tung"
+ *          index_read = 1 => string_read = "van tuan"
+ *          index_read = 3 => string_read = "obama"
+ */
+void readCSV_Variable(string line, int &index_read, string &string_read) {
+    if (line[index_read] == file_string_quote) {
+        readCSV_VariableQuote(line, index_read, string_read);
+    }
+    else readCSV_VariableNormal(line, index_read, string_read);
+}
+/**
+ * Read lines in file scv
+ * Example file csv:    "hoc", cách, làm, "trò chơi"
+ *                      "cuộc", sống, ôi, "đẹp quá"
+ * string_lines = [
+ *      0 => string("hoc", cách, làm, "trò chơi")
+ *      1 => string("cuộc", sống, ôi, "đẹp quá")
+ * ]
+ */
+int readCSV_ToLines(string *&string_lines, bool &read_error) {
+    ifstream file(file_src);
+    int lines_count = 0;
+    if (file.is_open()) {
+        /* End get lines count */
+        string_lines = new string[1];
+        while (!file.eof()) {
+            getline(file, string_lines[lines_count]);
+            lines_count++;
+            allocate_memory_string(string_lines, lines_count);
+            // allocate new memory (+1 type)
+        }
+        file.close();
+    }
+    else {
+        read_error = true;
+    }
+    return lines_count;
+}
+
+#endif //DO_AN_CUOI_KY_READ_CSV_H
